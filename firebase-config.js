87f047c9985dd47488f4c0e1baa95d1664c70983
jasim28v-cloud <jasim28v@gmail.com>
2026-03-26 00:18:53 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVyQS6kuOBDyx_FXoGx6xgXxbbzjs5COg",
+    authDomain: "fokx-c135a.firebaseapp.com",
+    databaseURL: "https://fokx-c135a-default-rtdb.firebaseio.com",
+    projectId: "fokx-c135a",
+    storageBucket: "fokx-c135a.firebasestorage.app",
+    messagingSenderId: "447033620521",
+    appId: "1:447033620521:web:730fde0a59cb5624128438"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
+
+const CLOUD_NAME = 'dk5kas1gc';
+const UPLOAD_PRESET = 'go_kck';
+
+console.log('✅ TikTok Clone Ready');
